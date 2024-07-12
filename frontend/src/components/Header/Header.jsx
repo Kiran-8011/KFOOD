251e0ee0b86622d405582a73e46acb189a16a0b9
Kiran <kiran8011gupta@gmail.com>
2024-07-13 00:36:40 +0530

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'> 
+      <div className="header-contents">
+        <h2>Order Now!</h2>
+        <p>Choose from a diverse menu featuring dishes crafted with finest ingredients and culinary expertise.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
